f2c416cbf01a84028bd0ac8e29e752d891e28edb
Morgan Fudge <mfudge92@gmail.com>
2017-11-11 19:57:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,10 +5,12 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = Array.new
   end
 
   def add_item(title, price, quantity = 1)
     @total += price * quantity
+    @items << 
   end
 
   def apply_discount
